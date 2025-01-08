0a46f806fd69f2d2b1b4704cb1fb5f67e07a6ce6
adriantech-beep <Adhrianne29@gmail.com>
2025-01-08 20:58:47 +0800

    updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
